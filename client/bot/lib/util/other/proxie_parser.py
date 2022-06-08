fdb2ac6a8a338513697351b4649a1e0d2dbb0d6c
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-07 23:39:16 -0400

    ajout de documentation et de fonction de comparaision pour les proxy

--- client/bot/lib/util/other/proxie_parser.py
+++ client/bot/lib/util/other/proxie_parser.py
@@ -3,10 +3,22 @@ from requests.auth import HTTPProxyAuth
 from random import choice
 
 def parseProxy(p:str):
+    """
+    It takes a string of the form "ip:port:user:password" and returns a Proxy object with the
+    corresponding values
+    
+    :param p: the proxy string
+    :type p: str
+    :return: A Proxy object
+    """
     ip,port,user,password=p.split(":")
     return Proxy(ip,port,user,password)
 
 def load_proxy():
+    """
+    It takes a list of proxies from a file, and returns a list of proxies
+    :return: A list of proxies
+    """
     list=[]
     for proxy in file_to_list(file_proxy):
         list.append(parseProxy(proxy))
@@ -14,7 +26,9 @@ def load_proxy():
     return list
 
 class Proxy:
-    
+    """
+    It's a class that creates a proxy object that can be used in requests
+    """
     def __init__(self,ip,port,user,passw) -> None:
         self.ip=ip
         self.port=port
@@ -25,28 +39,28 @@ class Proxy:
         self.__init__(self,ip,port,user,passw)
         self.authUser,self.authPass=auth
         
-    
     def proxy(self):
         return  {
                 'http': 'http://{}:{}@{}:{}'.format(self.user, self.passw, self.ip, self.port),
                 'https': 'http://{}:{}@{}:{}'.format(self.user, self.passw, self.ip, self.port)
-            }
-        
+            }    
         
     def get_auth(self,):
         return HTTPProxyAuth(username=self.authUser,password=self.authPass)
-    def get_proxy(self)->dict:
-        return {
-            #'https':f"https://{self.ip}:{self.port}",
-            'http':f"http://{self.ip}:{self.port}"
-            }
+        
     def get_proxyUrl(self):
         return f'http://{self.user}:{self.passw}@{self.ip}:{self.port}'
+    
     def get_fullProxy(self):
         return {'http':self.get_proxyUrl()}
+    
     def __repr__(self) :
         return self.get_proxyUrl()
     
+    def __str__(self) -> str:
+        return self.get_proxyUrl()
     
+    def __eq__(self, __o: object) -> bool:
+        return self.__str__() == __o.__str__()
         
 
