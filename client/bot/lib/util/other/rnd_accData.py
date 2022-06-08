81abdb5849667c7f15cab6481cda6175de973625
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-08 07:15:02 -0400

    ajout de documentation

--- client/bot/lib/util/other/rnd_accData.py
+++ client/bot/lib/util/other/rnd_accData.py
@@ -16,10 +16,27 @@ class BirthFormat(Enum):
 
 
 def gen_birthday(birthday_type:BirthFormat):
+    """
+    > This function returns a random birthday in the format of `mm/dd/yyyy`
+
+    The function body is made up of the following parts
+    
+    :param birthday_type: This is the type of birthday you want to generate
+    :type birthday_type: BirthFormat
+    :return: A string in the format of MM/DD/YYYY
+    """
     if birthday_type==BirthFormat.FOOTSITE: 
         return f"{format_date(12)}/{format_date(29)}/{randint(1980,2001)}"
 
 def format_date(max:int):
+    """
+    It returns a random integer between 1 and the value of the parameter max, formatted as a two digit
+    number
+    
+    :param max: the maximum number of days in the month
+    :type max: int
+    :return: A string of two digits.
+    """
     return format(randint(1,max),"02d")
 
 
@@ -33,4 +50,8 @@ def gen_password():
         ''.join(choice(digits) for x in range(3))).__add__(choice(special_char))
     
 def gen_number():
+    """
+    It generates a random 5 digit number.
+    :return: A string of 5 digits.
+    """
     return ''.join(choice(digits)for x in range(5))    
