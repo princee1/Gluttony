b6818efbad1c2d5f39b4ffe6ae70f735e8b6847b
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-08 06:50:48 -0400

    ajout de method au class de requetes

--- client/bot/lib/module/footsites/footsite_request/footsites_session.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_session.py
@@ -1,10 +1,27 @@
+from urllib.request import proxy_bypass
+from client.bot.lib.util.other.proxie_parser import Proxy
 from util.taskmanager.requestable_interface import Requestable
 import headers_gen as hg
 from requests import Session
+from enum import Enum
 
+
+
+
+##TODO: completer l'objet footsite session
+datadome_url = "https://api-js.datadome.co/js/"
+datadome_api_key = 'A55FBF4311ED6F1BF9911EB71931D5'
+
+session_url_champs = "https://www.champssports.ca/api/v4/session"
+auth_url_champs = 'https://www.champssports.ca/api/v4/auth'
+
+
+class FootsiteType(Enum):
+    CHAMPS=1
+    FOOTLOCKER=2
+    pass
 
 class FootsiteSession(Requestable,Session):
-    
     pass
 
 class FoositeAuth(FootsiteSession):
--- client/bot/lib/util/taskmanager/requestable_interface.py
+++ client/bot/lib/util/taskmanager/requestable_interface.py
@@ -1,3 +1,16 @@
+##TODO: Trouver des meilleurs nom de methodes
+from util.other.proxie_parser import Proxy
+
 
 class Requestable:
+    def __init__(self,proxy:Proxy,useragents):    
+        pass
+    
+    def rotateProxies(self):
+        pass
+    def start(self):
+        pass
+    def end(self):
+        pass
+    
     pass
