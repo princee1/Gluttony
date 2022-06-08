bf5e9a9d6f073cdb0a369f520b43f69ad1900678
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-08 16:36:37 -0400

    ajout de classes pour les donne en csv de footsite

---
+++ client/bot/lib/module/footsites/common/footsties_csvData.py
@@ -0,0 +1,14 @@
+from util.fileInterface.csv_interface import CSV_Data
+import util.fileInterface.csv_interface as csvi
+
+class Footsite_CSV(CSV_Data): 
+    pass
+
+class Footsite_Account_CSV(Footsite_CSV): 
+    pass
+
+class Footsite_Entries_CSV(Footsite_CSV): 
+    pass
+
+class Footsite_Win_CSV(Footsite_CSV): 
+    pass
