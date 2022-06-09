d82a46d23f2830a8b5080502d0a692fe259b5ed4
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-08 23:44:25 -0400

    fonction pour ecrire rapidement dans un  .txt

--- client/bot/lib/util/fileInterface/text_interface.py
+++ client/bot/lib/util/fileInterface/text_interface.py
@@ -21,7 +21,7 @@ def file_to_list(file: str):
         list.append(x[0:size])
         cpt += 1
     reader.close()
-    
+
     return list
 
 def write_list(file: str, data: list):
@@ -34,13 +34,15 @@ def write_list(file: str, data: list):
 def deleteElement_inFile(element:str,file:str):
     #raise NotImplemented
     list=file_to_list(file)
-    
     #print(list)
     for x in range(0,list.count(element)):
         list.remove(element)
     #print(list)
     write_list(file,list)
     
+def writeBufferFile(filename:str,buffer:str,inputMode):
+    with open(filename,f"{inputMode}") as file:
+        file.write(buffer)
+        file.close()
+        pass
 
-
-#deleteElement_inFile('''JMFbVH50XMWSD7JSeLxAF88CPIT9GWbAP7Op5BE2ZIA/kIQhWVsTquWsdW7puzmB3g77glWWglAwlq8/TwPDw2kSXQ1GFAqAkFhid8Jyd0foeCNx+3r66oKGDnmjMa3QOKbOU9uA89BK4jmVUym24Tq/lS8/mUF1eZPg32XnzGs=''',email)
