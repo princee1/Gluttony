1c82634abaf5dcb30c61e5e9eab92a1db9148cb1
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-09 21:18:08 -0400

    completion du module automatic update proxy list

--- client/bot/lib/util/proxyManager/proxyClipBoard.py
+++ client/bot/lib/util/proxyManager/proxyClipBoard.py
@@ -1,26 +1,50 @@
 import win32clipboard as clip
-from fileInterface.text_interface import writeBufferFile,proxy
-buffer:str=""
+import sys
+sys.path.insert(
+    1, "C:/Users/david/Documents/Gluttony Workspace/Gluttony/client/bot/lib/util/fileInterface/")
+# FIXME   arranger le module not found
+#from ui.print_cli import print_error,print_succes
 
-def getDataFromClipBoard():
+
+
+def getDataFromClipBoard() -> str:
     """
     It opens the Windows clipboard, gets the data, empties the clipboard, and closes the clipboard
     """
     clip.OpenClipboard()
-    buffer=clip.GetClipboardData()
+    buffer: str = clip.GetClipboardData()
     clip.EmptyClipboard()
     clip.CloseClipboard()
-    print(buffer)
+    #print(buffer)
     return buffer
+
+
+def writeInFile(buffer: str,inputMode):
+    """
+    It takes a string and a mode and writes the string to a file
     
-def writeProxyFromClipBoard():
-    proxies=getDataFromClipBoard()
-    writeBufferFile(proxy,proxies,"w")
-    pass
+    :param buffer: The string that you want to write in the file
+    :type buffer: str
+    :param inputMode: This is the mode in which the file is opened
+    """
+    writer = open("Proxy.txt", inputMode)
+    for x in buffer.split("\n"):
+        x = x.strip()
+        writer.write(x+"\n")
+    writer.close()
 
-def writeProxyFromClipBoardAppend():
-    proxies=getDataFromClipBoard()
-    writeBufferFile(proxy,proxies,"a")
-    pass
 
-getDataFromClipBoard()
+def writeProxyFromClipBoard(inputmode):
+    """
+    It takes the data from the clipboard and writes it to a file
+
+    :param inputmode: 
+    """
+    try:
+        proxies = getDataFromClipBoard()
+    except TypeError:
+        print("Clipboard is Empty")
+        pass
+    else:
+        writeInFile(proxies,inputmode)
+
