311036afd7a86ed0bd285b55965f7c24109b5da7
princee1 <89865089+princee1@users.noreply.github.com>
2022-06-13 13:05:47 -0400

    ajout de input for password et sytle

--- client/bot/lib/ui/question_ui.py
+++ client/bot/lib/ui/question_ui.py
@@ -1,53 +1,77 @@
 #from __future__ import print_function, unicode_literals
-from prompt_toolkit.styles.base import Style 
-from whaaaaat import prompt,style_from_dict
+from prompt_toolkit.styles.base import Style
+from whaaaaat import prompt, style_from_dict,Token,Separator
 from print_cli import print_principalOption
 
+#TODO sytle builder
 
-def selectOption(list:list,message):
-    question={
-        'type':'list',
-        'name':'option',
-        'message':message,
-        'choices':list
+style = style_from_dict({
+    Token.Separator: '#6C6C6C',
+    Token.QuestionMark: '#FF9D00 bold',
+    Token.Selected: '#001aff',  # default
+    #Token.Selected: '#5F819D',
+    Token.Pointer: '#FF9D00 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#5F819D bold',
+    Token.Question: '',
+})
+
+def selectOption(list: list, message) -> int:
+    question = {
+        'type': 'list',
+        'name': 'option',
+        'message': message,
+        'choices': list
     }
     return list.index(prompt(question).get('option'))
-    
-def checkBoxOption(list:list,message):
-    listDict=[]
+
+
+def checkBoxOption(list: list, message):
+    listDict = []
     for x in list:
-        listDict.append({'name':x})
+        listDict.append({'name': x})
 
-    question=[{
-        'type':'checkbox',
-        'name':'value',
-        'message':message,
-        'choices':listDict
+    question = [{
+        'type': 'checkbox',
+        'name': 'value',
+        'message': message,
+        'choices': listDict
     }]
-    return prompt(question).get('value')  
+    return prompt(question).get('value')
 
-def simpleQuestion(message:str):
-    question=[
-        {'type':'input',
-        'name':'value',
-        'message':message
-        }  
+
+def simpleQuestion(message: str):
+    question = [
+        {'type': 'input',
+         'name': 'value',
+         'message': message,
+         #'validator':lambda x:str(x)
+         }
     ]
     return prompt(question).get('value')
 
-def confirm(message):
-    question=[
+
+def confirm(message) -> bool:
+    question = [
         {
-            'type':'confirm',
-            'name':'value',
-            'message':message
-            
+            'type': 'confirm',
+            'name': 'value',
+            'message': message
         }
     ]
-   
+
     return prompt(question).get('value')
 
-def addSeparator(list,index):
+
+def enterPassWord(message):
+    question = [{
+        'type': 'password',
+        'name': 'value',
+        'message': message
+    }
+    ]
+    return prompt(question).get('value')
+    
+
+def addSeparator(list, index):
     pass
-
-
