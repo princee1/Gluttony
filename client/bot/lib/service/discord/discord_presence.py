6dadf354c8dff6ce9f065afb95108c79f2d042e6
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-25 10:40:36 -0500

    addig thread to change the time

--- client/bot/lib/service/discord/discord_presence.py
+++ client/bot/lib/service/discord/discord_presence.py
@@ -1,33 +1,64 @@
 from pypresence import *
-import time
-applicationID='929427634373394452'
-from print_cli import print_succes,print_error,print_desc
-import discord 
-
+import time,discord
+##from print_cli import success,error,desc
+import threading as td
 
 #TODO Implemeter le discord prescence
 
+APPLICATION_ID='929427634373394452'
+MAX_RETRY=5
+user_discordRP={"state":"Allo","details":"Allo","small-text":"allo","large-text":"allo"}
+presence=Presence(APPLICATION_ID)
 
+def connect_RP():
+   ## desc("Connecting to discord...")
+    print("Connecting to discord...")
+    count=1
+    connected=False
+    while (count!=MAX_RETRY):
+        try:
+            time.sleep(500)
+            presence.connect()
+            connected= True
+            ##success("\n * Discord Rich Presence Connected ! *\n")
+        except ValueError:
+           ## error("Aborting...")
+            count= 5
+        except KeyboardInterrupt:
+           ## error("Aborting...")
+            count =5
+        except:
+           ## error("* Error while connecting to discord presence *")
+            ##desc(f"Trying again{count}/{MAX_RETRY}")
+            count+=1
+        finally:
+            pass
+    else:
+        pass
+    return connected
 
-def discordRP():
-    
-    #print_desc("Connecting to discord...")
-    try:
-        presence=Presence(applicationID)
-        presence.connect()
-        presence.update(
-        details="Alpha Version",
-        large_text="Footsites Account Generator",
-        small_text="Version: 1.0.0" 
-        )
-        print_succes("\n * Discord Rich Presence Connected ! *\n")
-    except ValueError:
-        print_error("Aborting...")
-    except KeyboardInterrupt:
-        print_error("Aborting...")
-    except:
-        print_error("\n * Error while connecting to discord presence *\n")
-    #time.sleep(100)
+def update_RP():
+    SLEEP_TIME=1
+    while(True):
+        time.sleep(SLEEP_TIME)
+        try:
+            presence.update(state=user_discordRP["state"],
+                        details=user_discordRP["details"],
+                        large_text=user_discordRP["large_text"],
+                        small_text=user_discordRP["small_text"])
+        except:
+           ## error("* Error while updating presence*\nClosing the connection...")
+            presence.close()
+            pass
 
-        
+def updateValue(key,newValue):
+    user_discordRP.update(key,newValue)
+    pass
 
+def start_discordRP():
+    if(connect_RP()):
+        thread = td.Thread(target=update_RP,name="Update_RP")
+        thread.start()
+    else:
+        ##Error
+        pass
