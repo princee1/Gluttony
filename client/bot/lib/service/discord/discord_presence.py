48fa1b5be639de3875edfa9db69e9c01faff9fb5
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-27 15:49:06 -0500

    documentation added

--- client/bot/lib/service/discord/discord_presence.py
+++ client/bot/lib/service/discord/discord_presence.py
@@ -5,12 +5,18 @@ import threading as td
 
 #TEST  le discord prescence
 
+# Setting up the discord rich presence.
 APPLICATION_ID='929427634373394452'
 MAX_RETRY=5
 user_discordRP={"state":"Allo","details":"Allo","small-text":"allo","large-text":"allo"}
 presence=Presence(APPLICATION_ID)
 
 def connect_RP():
+    """
+    It tries to connect to discord rich presence, and if it fails, it tries again until it reaches the
+    maximum number of retries
+    :return: A boolean value.
+    """
     desc("Connecting to discord...")
     count=1
     connected=False
@@ -37,6 +43,9 @@ def connect_RP():
     return connected
 
 def update_RP():
+    """
+    It updates the discord rich presence every second.
+    """
     SLEEP_TIME=1
     while(True):
         time.sleep(SLEEP_TIME)
@@ -51,10 +60,20 @@ def update_RP():
             pass
 
 def updateValue(key,newValue):
+    """
+    It updates the value of a key in a dictionary
+    
+    :param key: The key of the value you want to update
+    :param newValue: The new value you want to update the key to
+    """
     user_discordRP.update(key,newValue)
     pass
 
 def start_discordRP():
+    """
+    It starts a thread that runs the update_RP function, which is a function that updates the discord
+    rich presence.
+    """
     if(connect_RP()):
         thread = td.Thread(target=update_RP,name="Update_RP")
         thread.start()
