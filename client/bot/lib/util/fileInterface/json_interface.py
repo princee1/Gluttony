7d8cbe93548b8eff028ad6becba37e444ef6c4de
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-26 22:05:15 -0500

    documentation added

--- client/bot/lib/util/fileInterface/json_interface.py
+++ client/bot/lib/util/fileInterface/json_interface.py
@@ -1,8 +1,12 @@
 import json5 as js
 
-
-
 def loadJson(file):
+    """
+    It tries to open the file, and if it can't, it returns None
+    
+    :param file: The file to load the JSON from
+    :return: A dictionary
+    """
     try:
         with open(file, "r") as fd:
             return js.load(fd, "utf-8")
@@ -10,13 +14,23 @@ def loadJson(file):
         return None
     
 def writeJson(json,file):
+    """
+    It takes a json object and a file name as input, and writes the json object to the file
+    
+    :param json: the json object to write to the file
+    :param file: The file to read from
+    """
     try:
         with open(file, "w") as fd:
             js.dump(json,fd)
     except:
         pass
-    
+
 class Settings():
+    """
+    It's a class that allows you to store and manipulate data in a json file
+    
+    """
     def __init__(self,module) -> None:
         self.modulePath=module
         self.settings:dict=loadJson(module)
