c13ab8db575cdd588980a80ceb3a1dd67db49859
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-28 14:22:48 -0500

    added method so each GluttonyException kid will overwite how to build their reason and their soution and create the solution

--- client/bot/lib/error/Gluttony_error.py
+++ client/bot/lib/error/Gluttony_error.py
@@ -1,4 +1,45 @@
 from builtins import BaseException
+from ui.print_cli import *
+
+ERROR_FILE:str
 
 class GluttonyException(BaseException):
-    pass
+    
+    def __init__(self, *args: object,errorCode,message,reason=None,solution=None) -> None:
+        super().__init__(*args) 
+        self.errorCode=errorCode
+        self.message=message
+        self.reason=reason
+        self.solution=solution
+        self.buildReason()
+        self.buildSolution()
+        
+    def applySolution(self):
+        pass
+    
+    def createSolution(self):
+        pass
+    
+    def createReason(self):
+        pass
+    
+    def buildReason(self):
+        if self.reason == None:
+            return
+        self.createReason()  
+        pass
+
+    def buildSolution(self):
+        if self.solution == None:
+            return
+        self.createSolution()  
+        pass
+
+    def logger(self):
+        
+        pass
+    
+    #TODO faire le format
+    def __repr__(self) -> str:
+        superRepr=super().__repr__()
+    
