00c128cad49a492f5b216c7985cb63a6e0c52fb6
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-28 11:16:40 -0500

    for the moment only the parent class will be implement cause only gmail works atm

---
+++ client/bot/lib/service/email/emailConnector.py
@@ -0,0 +1,15 @@
+class EmailConnector():
+    def __init__(self) -> None:
+        pass
+    
+    def login(self):
+        pass
+    
+    def extractMail(self):
+        pass
+        
+    def extractData():
+        pass
+    
+    def saveData(self):
+        pass
