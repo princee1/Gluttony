b855477844c49ccfd51dd95ca82a89dec41888cc
princee1 <89865089+princee1@users.noreply.github.com>
2022-12-31 19:59:00 -0500

    added behavior to the EmailConn

--- client/bot/lib/service/email/emailConnector.py
+++ client/bot/lib/service/email/emailConnector.py
@@ -11,8 +11,11 @@ class EmailConn():
     def extractMail(self,mailBox,fromWho,subject):
         pass
     
-    def extractData(self):
+    def extractData(self,handler,*args):
+        
         pass
     
-    def saveData(self,filename):
+    def saveData(self,filename,handler,*args):
+        self.extractData(handler,args)
+        #TODO saving the data in the file
         pass
