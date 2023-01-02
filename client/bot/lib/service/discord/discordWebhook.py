b6d11652e992883f04c08ef427145f043527ce27
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 13:58:13 -0500

    added documentation

--- client/bot/lib/service/discord/discordWebhook.py
+++ client/bot/lib/service/discord/discordWebhook.py
@@ -6,6 +6,15 @@ USERNAME_WEBHOOOK="Gluttony"
 AVATAR_URL=""
 
 def sendWebhook(disEmbed,webhookURL,content,id=None):
+    """
+    It takes a list of DiscordEmbed objects, a webhook URL, a content string, and an optional ID, and
+    sends the webhook.
+    
+    :param disEmbed: A list of discord embeds
+    :param webhookURL: The webhook URL
+    :param content: The message content
+    :param id: The id of the webhook
+    """
     webhook = DiscordWebhook(webhookURL,id,content,USERNAME_WEBHOOOK,AVATAR_URL
     )
     (webhook.add_embed(embed) for embed in disEmbed)
@@ -19,7 +28,13 @@ def sendWebhook(disEmbed,webhookURL,content,id=None):
     pass
 
 def buildEmbed(title,description):
+    """
+    This function takes a title and description and returns a DiscordEmbed object
+    
+    :param title: The title of the embed
+    :param description: The description of the embed
+    :return: A DiscordEmbed object
+    """
     d=DiscordEmbed(title,description )
     
     return d
-    pass
