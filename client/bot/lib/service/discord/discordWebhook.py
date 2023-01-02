129fcf8d53cca712a568509c64726f722c992b4b
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 13:56:22 -0500

    added method to send webhook

--- client/bot/lib/service/discord/discordWebhook.py
+++ client/bot/lib/service/discord/discordWebhook.py
@@ -1,2 +1,25 @@
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook,DiscordEmbed
+import usermanager.user_manager as user
+import time
 
+USERNAME_WEBHOOOK="Gluttony"
+AVATAR_URL=""
+
+def sendWebhook(disEmbed,webhookURL,content,id=None):
+    webhook = DiscordWebhook(webhookURL,id,content,USERNAME_WEBHOOOK,AVATAR_URL
+    )
+    (webhook.add_embed(embed) for embed in disEmbed)
+    webhook.allowed_mentions=[  
+    ]
+    try:
+        response=webhook.execute()
+    except:
+        print("Failed to execute webhook")
+    
+    pass
+
+def buildEmbed(title,description):
+    d=DiscordEmbed(title,description )
+    
+    return d
+    pass
