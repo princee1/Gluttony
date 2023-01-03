a0d109fe48bb66a4dc3193e5c13a6069e0865b92
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 19:57:50 -0500

    added a webhook enum

--- client/bot/lib/util/constant.py
+++ client/bot/lib/util/constant.py
@@ -5,9 +5,13 @@ domain = "Domains.txt"
 proxy = "Proxy.txt"
 rnd_lname = "randomlname.txt"
 rnd_fname = "randomfname.txt"
-
 user_agents="user-agents.txt"
 
 class ModuleEnum(Enum):
     FOOTSITE=0
     ADIDAS=1
+    
+class WebhookEnum(Enum):
+    DISCORD=0
+    AYCD=1
+    
