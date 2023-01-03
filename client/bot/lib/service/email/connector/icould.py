1914d82c4a8d0173c88bda37dffe62e226403012
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 20:53:05 -0500

    added implecmentation for the icloud and outlook email connector

--- client/bot/lib/service/email/connector/icould.py
+++ client/bot/lib/service/email/connector/icould.py
@@ -1,1 +1,16 @@
+from emailConnector import EmailConn
 raise NotImplementedError()
+
+class Icloud(EmailConn):
+    
+    def login(self):
+        pass
+    
+    def extractData(self, handler, *args):
+        pass
+    
+    def extractMail(self, mailBox, fromWho, subject):
+        pass    
+    def saveData(self, filename, handler, *args):
+        return super().saveData(filename, handler, *args)
+    pass
--- client/bot/lib/service/email/connector/outlook.py
+++ client/bot/lib/service/email/connector/outlook.py
@@ -1,4 +1,17 @@
 import win32com.client.gencache as win
+from emailConnector import EmailConn
 
 #NOTE email, read from oulook client application
 raise NotImplementedError()
+class Outlook(EmailConn):
+    def login(self):
+        pass
+    
+    def extractData(self, handler, *args):
+        pass
+    
+    def extractMail(self, mailBox, fromWho, subject):
+        pass    
+    def saveData(self, filename, handler, *args):
+        return super().saveData(filename, handler, *args)
+    pass
