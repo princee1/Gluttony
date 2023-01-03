64f6775541195363264c72d7106218f4bf2cc318
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 19:43:51 -0500

    added a win representation class

---
+++ client/bot/lib/util/other/winResponse.py
@@ -0,0 +1,18 @@
+from file_manager import writeFile
+
+class Win():
+    
+    def __init__(self,response):
+        self.deconstruct(response)
+        pass
+    
+    def deconstruct(self,response):
+        pass
+    
+    def toFile(self,file,buffer):
+        writeFile(file,buffer,"a")
+       
+    def toDiscordEmbedValue() -> tuple:
+        pass
+    
+    pass
