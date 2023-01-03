bf034ea65d57a072f71ebc2e070575e1e4c1ec11
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-02 19:57:30 -0500

    added a sendwebhook method and aycdParser

--- client/bot/lib/util/other/winResponse.py
+++ client/bot/lib/util/other/winResponse.py
@@ -1,7 +1,10 @@
 from file_manager import writeFile
+from service.discord.discordWebhook import sendWebhook,buildEmbed
+from constant import WebhookEnum
+#NOTE import the aycd module
 
 class Win():
-    
+
     def __init__(self,response):
         self.deconstruct(response)
         pass
@@ -13,6 +16,20 @@ class Win():
         writeFile(file,buffer,"a")
        
     def toDiscordEmbedValue() -> tuple:
+        pass
+    
+    def toAycdValue()->tuple:
+        pass
+    
+    def sendWebhook(self,webhook:WebhookEnum,id,url,content):
+        if webhook==WebhookEnum.DISCORD:
+            values=self.toDiscordEmbedValue()
+            disEmbed=buildEmbed()
+            sendWebhook(url,content,id,disEmbed)
+            pass
+        else:
+            raise NotImplementedError()
+            pass
         pass
     
     pass
