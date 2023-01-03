d45cc6cb560cb09703cbb7ad69de050a75f45846
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-03 02:59:20 -0500

    added method to the task class

--- client/bot/lib/util/taskmanager/task.py
+++ client/bot/lib/util/taskmanager/task.py
@@ -2,15 +2,23 @@ from requestable_interface import Requestable
 from threading import Thread
 
 
+
 class Task(Thread):
-    def __init__(self,request:Requestable):
+    def __init__(self,request:Requestable,index:int,semOut,semData):
         super().__init__(self,daemon=False)
         self.request = request
+        self.index=index
         
     def  run(self):
         pass
     
+    def request(self):
+        pass
+    
     def end(self):
+        pass
+    
+    def communicate(self):
         pass
     
     def __repr__(self) -> str:
