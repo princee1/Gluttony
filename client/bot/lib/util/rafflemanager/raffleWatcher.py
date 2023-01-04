217b9fa1d64c665b15047a70ec3484fec95fc55e
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 01:13:30 -0500

    added documenation and changing a function name

--- client/bot/lib/util/rafflemanager/raffleWatcher.py
+++ client/bot/lib/util/rafflemanager/raffleWatcher.py
@@ -3,6 +3,9 @@ from raffle import Raffle
 from math import ceil,floor
 
 
+# The RaffleTimer class is a subclass of the Timer class. It has a constructor that takes in a Raffle
+# object and a process to start. It has a run method that prints out the raffle and the time until the
+# raffle starts. It has a target method that manipulates the process to start
 class RaffleTimer(Timer):
     def __init__(self,raffle,processToStart):
         self.process=processToStart
@@ -13,7 +16,7 @@ class RaffleTimer(Timer):
     def run(self) -> None:
         print("  Watching the Raffle:")
         print(self.raffle)
-        print(f"Raffle starts in {convertToSeconds(self.interval)}")
+        print(f"Raffle starts in {convertTime(self.interval)}")
         super().run()
         
     def target(self):
@@ -21,7 +24,12 @@ class RaffleTimer(Timer):
         pass
 
 
-def convertToSeconds(time):
+def convertTime(time):
+    """
+    It converts a time in seconds to hours, minutes and seconds.
+    
+    :param time: The time in seconds
+    """
     h= (time / 3600)
     m = (round(h-floor(h),5) *3600)/60
     s = (m-floor(m))*60
