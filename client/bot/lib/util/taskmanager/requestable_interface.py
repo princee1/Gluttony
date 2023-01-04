1d42c2102d6b6e1d6e531467a4e7bc81b62e34a1
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 02:06:33 -0500

    added some method

--- client/bot/lib/util/taskmanager/requestable_interface.py
+++ client/bot/lib/util/taskmanager/requestable_interface.py
@@ -4,14 +4,18 @@ from util.other.proxie_parser import Proxy
 
 
 class Requestable(Session):
-    def __init__(self,proxy:Proxy,useragents):    
+    def __init__(self,proxy:Proxy,useragents):  
+        self.proxies=proxy.proxy()
         pass
-    
-    def rotateProxies(self):
+    def rotateProxies(self,proxy):
+        self.proxies=proxy
         pass
     def start(self):
         pass
-    def end(self):
+    def restart():
         pass
+    def end(self):
     
+        pass
+     
     pass
