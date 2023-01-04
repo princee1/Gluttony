51a887c58aa454fb77a42205d35325e43110ec80
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 02:07:38 -0500

    added a request state

--- client/bot/lib/util/taskmanager/requestable_interface.py
+++ client/bot/lib/util/taskmanager/requestable_interface.py
@@ -1,7 +1,10 @@
 ##TODO: Trouver des meilleurs nom de methodes
 from requests import Session
+from enum import Enum
 from util.other.proxie_parser import Proxy
 
+class RequestState(Enum):
+    pass
 
 class Requestable(Session):
     def __init__(self,proxy:Proxy,useragents):  
