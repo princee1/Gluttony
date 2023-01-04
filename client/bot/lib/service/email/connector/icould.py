762474bf03c2b044d9e8ecdc80ac2d5a5158e528
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 13:37:02 -0500

    imported modules for connecting into icloud services

--- client/bot/lib/service/email/connector/icould.py
+++ client/bot/lib/service/email/connector/icould.py
@@ -1,15 +1,19 @@
 from emailConnector import EmailConn
+from pyicloud import PyiCloudService 
 raise NotImplementedError()
 
 class Icloud(EmailConn):
     
-    def login(self):
+    def login(self,user,passw):
+        self.icloudService=PyiCloudService(user,passw)
         pass
     
     def extractData(self, handler, *args):
         pass
     
     def extractMail(self, mailBox, fromWho, subject):
+        tempMails=self.icloudService.files['com~apple~mail']
+        
         pass    
     def saveData(self, filename, handler, *args):
         return super().saveData(filename, handler, *args)
