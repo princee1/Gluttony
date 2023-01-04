d4e8fe65c3e0ad5a976f7b003e5ddb10da820c98
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 01:11:52 -0500

    added a class that start the process raffle after a certain time

--- client/bot/lib/util/rafflemanager/raffleWatcher.py
+++ client/bot/lib/util/rafflemanager/raffleWatcher.py
@@ -1,3 +1,28 @@
 from threading import Timer
-import time
+from raffle import Raffle
+from math import ceil,floor
 
+
+class RaffleTimer(Timer):
+    def __init__(self,raffle,processToStart):
+        self.process=processToStart
+        self.raffle:Raffle= raffle
+        super().__init__(self.raffle.getElapsedTime(),self.target)
+        
+        
+    def run(self) -> None:
+        print("  Watching the Raffle:")
+        print(self.raffle)
+        print(f"Raffle starts in {convertToSeconds(self.interval)}")
+        super().run()
+        
+    def target(self):
+        #TODO manipulate the process to start
+        pass
+
+
+def convertToSeconds(time):
+    h= (time / 3600)
+    m = (round(h-floor(h),5) *3600)/60
+    s = (m-floor(m))*60
+    return f"{floor(h)}(h) - {floor(m)}(min) - {round(s,1)}(sec)"
