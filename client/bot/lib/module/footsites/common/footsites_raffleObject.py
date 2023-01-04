6db3a5418a69789993cca3253c5d8f10a94c191f
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 00:13:24 -0500

    added documentaion

--- client/bot/lib/module/footsites/common/footsites_raffleObject.py
+++ client/bot/lib/module/footsites/common/footsites_raffleObject.py
@@ -4,7 +4,10 @@ from util.rafflemanager.raffle import Raffle
 WINFILE:str
 
 
-class FootSiteRaffle(Raffle):
+# > The `FootsiteRaffle` class is a subclass of the `Raffle` class. It has a constructor that takes a
+# `response` object as an argument. It has a method called `deconstruct` that takes a `value` as an
+# argument
+class FootsiteRaffle(Raffle):
     def __init__(self,response):
         self.deconstruct(response)
         pass
@@ -15,6 +18,10 @@ class FootSiteRaffle(Raffle):
     pass
 
 
+# `FootsitesWin` is a subclass of `Win` that has a `deconstruct` method that takes a `response` as an
+# argument and returns nothing, a `toFile` method that takes a `buffer` as an argument and returns the
+# result of calling the `toFile` method of the `Win` class, and a `toDiscordEmbedValue` method that
+# returns a tuple
 class FootsitesWin(Win):
     
     def deconstruct(self, response):
