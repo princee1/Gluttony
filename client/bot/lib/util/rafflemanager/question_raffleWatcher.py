529073a82fd2573c4b9360cd5f0a3bfa9a9ca0bb
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-04 00:25:19 -0500

    added a function thaht get the choosed raffle added some doc

--- client/bot/lib/util/rafflemanager/question_raffleWatcher.py
+++ client/bot/lib/util/rafflemanager/question_raffleWatcher.py
@@ -1,5 +1,13 @@
 
 def getRaffleChoices(listRaffle,listIndex):
+    """
+    It takes a list of raffle numbers and a list of indexes and returns a list of raffle that contains only 
+    the ones that the user choosed
+    
+    :param listRaffle: The list of all the raffle choices
+    :param listIndex: list of indexes of the listRaffle
+    :return: A list of the raffle choices
+    """
     listTemp=[]
     for x in listIndex:
         listTemp.append(listRaffle[x])
