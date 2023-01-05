5a0738c63dc91fb49257239b45fc3fbd92aa5c02
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 15:37:08 -0500

    put the module Request implementation in the requestable interface since itll be the same for everyone

--- client/bot/lib/util/taskmanager/requestable_interface.py
+++ client/bot/lib/util/taskmanager/requestable_interface.py
@@ -12,11 +12,12 @@ map_statusCode_error = {
     503: "First byte timeout"
 }
 class RequestState(Enum):
-    ERROR=0,
-    SUCCESS=1,
-    PROXY_ERROR=2,
-    RETRY=3,
-    NEXT=4,
+    INIT=0,
+    ERROR=1,
+    SUCCESS=2,
+    PROXY_ERROR=3,
+    RETRY=4,
+    NEXT=5,
     
     pass
 
@@ -31,6 +32,44 @@ class Requestable(Session):
         pass
     def start(self):
         pass
+    
+    def moduleRequest(self,url,method,data,successCode,succesText):
+        """
+        It takes a url, method, data, successCode, and successText and returns a boolean and a string.
+        
+        :param url: The url to send the request to
+        :param method: POST or GET
+        :param data: is a dictionary of the data that is being sent to the server
+        :param successCode: The code that the server returns when the request is successful
+        :param succesText: The text that should be in the response if the request was successful
+        :return: The returnState function is being called with the parameters successCode,
+        self.response, and succesText.
+        """
+        try:
+            response =self.request(method,url,headers=self.headers
+                         ,cookies=self.cookies,
+                         proxies=self.proxies,
+                         json=data)   
+        except KeyError: 
+            return False, "Couldnt connect to the Server"
+        except JSONDecodeError:
+            return False, "Couldnt get a session"
+        except TimeoutError:
+            return False, "The handshake operation timed out"
+        except ProxyError:
+            return False, "Cannot connect to proxy"
+        except SSLError:
+            return False, "Permissions error"
+        except ReadTimeout:
+            return False, "Timeout Error"
+        pass
+        self.response=response
+        del response
+        return returnState(successCode,self.response,succesText)
+    
+    def authenticate(self) -> None:
+        pass
+    
     def restart():
         pass
     def end(self):
