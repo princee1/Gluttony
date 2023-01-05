d7ba2d93d5382fe9c902f307a71210ef042828fd
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 14:29:56 -0500

    added FootsiteCreateAccount request implementation

--- client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
@@ -1,6 +1,16 @@
 from footsites_session import FootsiteSession,FoositeAuth
+from common.footsites_account import FootsiteAccount
+
+account_url = "https://www.champssports.ca/api/v4/users"
 
 class FoostiteCreateAccount(FootsiteSession):
+    
+    def __init__(self, proxy, useragents,account):
+        super().__init__(proxy, useragents)
+        self.account:FootsiteAccount = account
+    
+    def moduleRequest(self):
+        return super().moduleRequest(account_url, "POST", self.account.to_json(), 201, "Account Sucessfully Created !")
     pass
 
 class FoostiteConfirmAccount(FoositeAuth):
