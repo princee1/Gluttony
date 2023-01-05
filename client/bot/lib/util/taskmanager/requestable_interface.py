9badee9cd567af36989fbb38bb8ef3cc08b10e21
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 03:16:47 -0500

    added State values to the RequestState Enum class

--- client/bot/lib/util/taskmanager/requestable_interface.py
+++ client/bot/lib/util/taskmanager/requestable_interface.py
@@ -3,12 +3,19 @@ from requests import Session
 from enum import Enum
 
 class RequestState(Enum):
+    ERROR=0,
+    SUCCESS=1,
+    PROXY_ERROR=2,
+    RETRY=3,
+    NEXT=4,
+    
     pass
 
 class Requestable(Session):
     def __init__(self,proxy,useragents):  
         self.proxies=proxy.proxy()
         self.useragents=useragents
+        self.state:RequestState
         pass
     def rotateProxies(self,proxy):
         self.proxies=proxy
