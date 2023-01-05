693c5b7b0317e50175f16db7b1fe1817dd7af94f
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 14:37:05 -0500

    added Confirm request implemetation

--- client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
@@ -2,6 +2,8 @@ from footsites_session import FootsiteSession,FoositeAuth
 from common.footsites_account import FootsiteAccount
 
 account_url = "https://www.champssports.ca/api/v4/users"
+activation_link = "https://www.champssports.ca/api/v4/activation"
+
 
 class FoostiteCreateAccount(FootsiteSession):
     
@@ -13,7 +15,21 @@ class FoostiteCreateAccount(FootsiteSession):
         return super().moduleRequest(account_url, "POST", self.account.to_json(), 201, "Account Sucessfully Created !")
     pass
 
-class FoostiteConfirmAccount(FoositeAuth):
+class FoostiteConfirmAccount(FootsiteSession):
+  
+    def __init__(self, proxy, useragents,token):
+        super().__init__(proxy, useragents)
+        self.acctoken=token
+    
+    def moduleRequest(self):
+        values= super().moduleRequest(activation_link, "POST", self.acctoken.to_json(), 200, "Account Succesfully Confirmed")
+        try:
+            if self.response.json()["activationStatus"] == "tokenExpired":
+                return None, f"Activation Token expired - {self.response.json()['userId']}"
+        except:
+            pass
+        return values
+    
     pass
 
 class FoostiteRenameAccount(FoositeAuth):
