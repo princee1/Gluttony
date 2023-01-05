f699a2766aded91ada999298e5e2962ad5a7d0bb
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 14:22:20 -0500

    all class extends either Footsite Session or Footsite Auth

--- client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
@@ -1,13 +1,13 @@
 from footsites_session import FootsiteSession,FoositeAuth
 
-class FoostiteCreateAccount():
+class FoostiteCreateAccount(FootsiteSession):
     pass
 
-class FoostiteConfirmAccount():
+class FoostiteConfirmAccount(FoositeAuth):
     pass
 
-class FoostiteRenameAccount():
+class FoostiteRenameAccount(FoositeAuth):
     pass
 
-class FoostiteIds():
+class FoostiteIds(FoositeAuth):
     pass
