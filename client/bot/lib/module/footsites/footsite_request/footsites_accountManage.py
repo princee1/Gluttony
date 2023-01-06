4e140b6f90dd73b6550f22df5fe3d31c0f2dd7c3
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 21:53:45 -0500

    added implementation

--- client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
@@ -1,8 +1,11 @@
 from footsites_session import FootsiteSession,FoositeAuth
 from common.footsites_account import FootsiteAccount
+from footsites_session import Footsite_CSV
 
 account_url = "https://www.champssports.ca/api/v4/users"
 activation_link = "https://www.champssports.ca/api/v4/activation"
+resentCode_url = 'https://www.champssports.ca/api/v4/users/activation/resend-code'
+
 
 
 class FoostiteCreateAccount(FootsiteSession):
@@ -33,10 +36,20 @@ class FoostiteConfirmAccount(FootsiteSession):
     pass
 
 class FoostiteRenameAccount(FoositeAuth):
+    def __init__(self, proxy, useragents, footstiteData,account:Footsite_CSV):
+        super().__init__(proxy, useragents, footstiteData)
+        self.account=account
+    
+    def moduleRequest(self):
+        return super().moduleRequest(account_url, "POST", self.account.login_data(), 201, "Name Changed and Saved")
     pass
 
+
 class FoostiteIds(FoositeAuth):
+    
     pass
 
 class FoostiteResendToken(FoositeAuth):
+    def moduleRequest(self, url, method, data, successCode, succesText):
+        return super().moduleRequest(url, method, data, successCode, succesText)
     pass
