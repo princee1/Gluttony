997a133c8b9b965bbcfde0d405b45ea73fa4804e
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-05 21:52:45 -0500

    refractoring the file

--- client/bot/lib/module/footsites/footsite_request/footsites_session.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_session.py
@@ -11,20 +11,17 @@ datadome_api_key = 'A55FBF4311ED6F1BF9911EB71931D5'
 session_url_champs = "https://www.champssports.ca/api/v4/session"
 auth_url_champs = 'https://www.champssports.ca/api/v4/auth'
 
-
 class FootsiteType(Enum):
     CHAMPS = 1
     FOOTLOCKER = 2
     pass
-
 
 class FootsiteSession(Requestable):
     """
     It's a class that creates a session with the Footsite, and then allows you to make requests to the
     Footsite
     """
-    
-    
+     
     def __init__(self, proxy, useragents):
         """
         The function gets the datadome cookie and crsf token from the website
@@ -96,40 +93,7 @@ class FootsiteSession(Requestable):
     
     def __repr__(self) -> str:
         return f"CSRF Token {self.cr} - Session Id {self.sessionid} "
-
-    def moduleRequest(self,url,method,data,successCode,succesText):
-        """
-        It takes a url, method, data, successCode, and successText and returns a boolean and a string.
-        
-        :param url: The url to send the request to
-        :param method: POST or GET
-        :param data: is a dictionary of the data that is being sent to the server
-        :param successCode: The code that the server returns when the request is successful
-        :param succesText: The text that should be in the response if the request was successful
-        :return: The returnState function is being called with the parameters successCode,
-        self.response, and succesText.
-        """
-        try:
-            response =self.request(method,url,headers=self.headers
-                         ,cookies=self.cookies,
-                         proxies=self.proxies,
-                         json=data)   
-        except KeyError: 
-            return False, "Couldnt connect to the Server"
-        except JSONDecodeError:
-            return False, "Couldnt get a session"
-        except TimeoutError:
-            return False, "The handshake operation timed out"
-        except ProxyError:
-            return False, "Cannot connect to proxy"
-        except SSLError:
-            return False, "Permissions error"
-        except ReadTimeout:
-            return False, "Timeout Error"
-        pass
-        self.response=response
-        del response
-        return returnState(successCode,self.response,succesText)
+      
     pass
 
 
@@ -150,8 +114,7 @@ class FoositeAuth(FootsiteSession):
         """
         super().__init__(proxy, useragents)
         self.footsiteData:Footsite_CSV=footstiteData
-
-
+        
     def authenticate(self):
         """
         It takes the response from the login request and updates the headers with the cookies from the
@@ -170,39 +133,5 @@ class FoositeAuth(FootsiteSession):
     pass
 
 
-def parse_error(response):
-    """
-    If the response is 400, return the error message from the response. Otherwise, return the error
-    message from the MAP_ERROR dictionary
-    
-    :param response: The response object from the request
-    :return: The response object
-    """
-    val = MAP_ERROR.get(response.status_code)
-    try:
-        if response.status_code == 400:
-            mess = response.json()["errors"][0]['message']
-            return f"{val}: {mess}"
-    except:
-        pass
-    return val
 
-
-def returnState(succes_code: int, respone, text: str):
-    """
-    If the status code of the response is equal to the success code, return True and the text, otherwise
-    return False and the parsed error
-    
-    :param succes_code: The HTTP status code that indicates success
-    :type succes_code: int
-    :param respone: The response from the API
-    :param text: The text to be sent to the user
-    :type text: str
-    :return: A tuple of two values.
-    """
-    status_code = respone.status_code
-    if status_code == succes_code:
-        return True, text
-    else:
-        return False, parse_error(respone)
 
