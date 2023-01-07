7fd78c103de4a17a19c2fcc567ca9bbf7adb8635
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-07 18:08:33 -0500

    added end method implementation to 2 classes

--- client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
+++ client/bot/lib/module/footsites/footsite_request/footsites_accountManage.py
@@ -1,5 +1,6 @@
 from footsites_session import FootsiteSession,FoositeAuth
 from common.footsites_account import FootsiteAccount,FoostiteActivationToken
+from common.footsties_csvData import Footsite_Account_CSV
 
 
 account_url = "https://www.champssports.ca/api/v4/users"
@@ -55,22 +56,40 @@ FoositeAuth class, but also takes in an account object. It then uses the account
 POST request to the account_url
 
     """
-    def __init__(self, proxy, useragents, footstiteData,account:Footsite):
+    FIRST:str
+    LAST:str
+    
+    def __init__(self, proxy, useragents, footstiteData,account:Footsite_Account_CSV):
         super().__init__(proxy, useragents, footstiteData)
         self.account=account
     
     def moduleRequest(self):
         return super().moduleRequest(account_url, "POST", self.account.login_data(), 201, "Name Changed and Saved")
+
+    def end(self):
+        self.account.updateName(self.FIRST,self.LAST)
+    
+    @classmethod
+    def changeName(self=None,first=None,last=None):
+        FoostiteRenameAccount.FIRST=first
+        FoostiteRenameAccount.LAST=last
+        
+        pass
     pass
+
 
 class FoostiteIds(FoositeAuth):
     """This class inherits from the FoositeAuth class and uses the moduleRequest method to make a request
 to the accountInfo_url and returns the response
 
     """
-    
     def moduleRequest(self):
         return super().moduleRequest(accountInfo_url, "GET", None, 200,"Ids Gathered And Saved!")
+    
+    def end(self):
+        self.footsiteData.customerID = self.response.json()["customerID"]
+        self.footsiteData.cCore = self.response.json()["cCoreCustomerId"]
+        ##self.footsiteData.updateValue("customerID", self.footsiteData.customerID)
     pass
 
 class FoostiteResendToken(FoositeAuth):
