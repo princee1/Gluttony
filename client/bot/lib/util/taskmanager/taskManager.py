8b039c5510a3fa7ed333cb8fe9df83aee81fa38f
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-07 23:15:00 -0500

    fixed some bugs, added some behavior and some method

--- client/bot/lib/util/taskmanager/taskManager.py
+++ client/bot/lib/util/taskmanager/taskManager.py
@@ -1,7 +1,7 @@
 from requestable_interface import Requestable,RequestState
 #from threading import BoundedSemaphore,Thread
 from taskCommunication import *
-
+from time import sleep
 
 MAX_ALIVE_TRHEAD=50
 N=1
@@ -11,12 +11,13 @@ manEvent = PEvent()
 
 class Task(Thread):
     def __init__(self,request:Requestable,index:int,semOut,semData,cEvent):
-        super().__init__(self,daemon=False)
+        super().__init__(None,daemon=False)
         self.request = request
         self.index=index
         self.semOut=semOut
         self.semData=semData
         self.cEvent:CEvent=cEvent
+        self.isDone=False
         
     def  run(self):
         
@@ -24,20 +25,29 @@ class Task(Thread):
             
             if self.request.state == RequestState.INIT:
                 pass
+            
             elif self.request.state == RequestState.AUTHENTICATED:
                 pass
-            elif self.request.state == RequestState.PROXY_ERROR:
-                pass
+            
             elif self.request.state == RequestState.SUCCESS:
                 pass
+            
+            elif self.request.state == RequestState.READY:
+                pass
+            
             elif self.request.state == RequestState.PAUSE:
                 pass
             
+            elif self.request.state == RequestState.PROXY_ERROR:
+                pass
+            
+            self.end()
             pass
         
         pass
     
     def end(self):
+        self.isDone=True
         pass
     
     def communicate(self,commMessage:CommMessage):
@@ -68,10 +78,16 @@ class TaskManager():
         self.taskList:list[Task] = taskList
         self.eventList:list[Event]= eventList
         self.activeTaskList:list[Task]=[]
-        self.thradComm=Thread(target=self.communicate)
+        self.threadComm=Thread(target=self.communicate)
     
     
-    def start(self):
+    def start(self):  
+        self.threadComm.start()
+        
+        for t in self.taskList:
+            t.start()
+        
+        self.waitThreads()    
         pass
     
     def waitThreads(self):
@@ -88,32 +104,45 @@ class TaskManager():
         The function waits for the event to be set, then it gets the index of the event, treats the
         communication, clears the event and releases the semaphore
         """
-        
-        manEvent.wait()
-        index=manEvent.index
-        self.treatComm(index)
-        manEvent.clear()
-        commSem.release(N)
+        while True: #NOTE when all thread are done
+            manEvent.wait()
+            mess=manEvent.commMessage
+            self.treatComm(mess)
+            manEvent.clear()
+            commSem.release(N)
         pass
     
-    def treatComm(self,index):
+    def treatComm(self,mess:CommMessage):
         """
         It takes a commMessage, treats it, and then sets the event that was waiting for it
         
         :param index: the index of the event in the eventList
         """
-
-        with manEvent.commMessage as cm:
+        if False:
             if cm.type==CommType.PROXY:
                 pass
             elif cm.type==CommType.PROXY_FLAG: 
                 pass
             #treating the commMessage
             pass 
-        self.eventList[index].set()
+        self.eventList[mess.index].set(Message())
         pass
     
-    def flagProcess():
+    def flagProcess(self):
+        pass
+    
+    def checkIsDone(self)->bool:
+        raise NotImplementedError()
+        complete=True
+        
+        for t in self.taskList:
+            if t.isDone !=True:
+                complete=False
+                return complete
+        
+        return complete
+    
+    def stats(self):
         pass
     
     pass
