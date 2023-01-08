04c574b7ddb18465a64931db56a2dc4dfec0e72e
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-07 21:59:58 -0500

    completed the class

--- client/bot/lib/util/taskmanager/taskCommunication.py
+++ client/bot/lib/util/taskmanager/taskCommunication.py
@@ -12,14 +12,22 @@ class CEvent(Event):
     def __init__(self,index) -> None:
         super().__init__()
         self.index=index
+        self.message=None
     
     def set(self,message:Message):
+        super().set()
         self.message=message
     pass
 
 class PEvent(Event):
     
+    def __init__(self):
+        super().__init__()
+        self.commType=None
+    
     def set(self,index,commType:CommType):
+        super().set()
         self.index=index
+        self.commType = commType
         pass
     pass
