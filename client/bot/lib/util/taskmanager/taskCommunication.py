73199c6a6e85bfb76d71e2e868b2812bee2d22bb
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-07 20:41:19 -0500

    added class thatll help the communication

--- client/bot/lib/util/taskmanager/taskCommunication.py
+++ client/bot/lib/util/taskmanager/taskCommunication.py
@@ -1,3 +1,14 @@
 from threading import Event
 from enum import Enum
 
+class CommType(Enum):
+    pass
+
+class Message():
+    pass
+
+class CEvent(Event):
+    pass
+
+class PEvent(Event):
+    pass
