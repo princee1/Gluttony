ccdf85b2a110243e1d7540b3b2fd5c11ae1ccbd5
princee1 <89865089+princee1@users.noreply.github.com>
2023-01-07 21:26:01 -0500

    added and modified some method

--- client/bot/lib/util/taskmanager/taskCommunication.py
+++ client/bot/lib/util/taskmanager/taskCommunication.py
@@ -1,4 +1,4 @@
-from threading import Event
+from threading import Event,BoundedSemaphore,Thread
 from enum import Enum
 
 class CommType(Enum):
@@ -9,13 +9,17 @@ class Message():
     pass
 
 class CEvent(Event):
+    def __init__(self,index) -> None:
+        super().__init__()
+        self.index=index
+    
     def set(self,message:Message):
         self.message=message
     pass
 
 class PEvent(Event):
     
-    def set(self,index):
+    def set(self,index,commType:CommType):
         self.index=index
         pass
     pass
